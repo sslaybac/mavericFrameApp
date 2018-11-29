262ad2cd1ee32b1ad2e70cf38efc5078f5f805f6
Scott Michael Slayback <Scott.M.Slayback@navy.mil>
2018-11-29 11:24:42 -0800

    simple app, aware of URL.

---
+++ app/src/main/java/maveric/collector/MainActivity.java
@@ -0,0 +1,16 @@
+package maveric.collector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, R.string.api_url, Toast.LENGTH_SHORT).show();
+    }
+}
