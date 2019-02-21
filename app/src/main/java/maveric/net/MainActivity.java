e13f400aebfda5946218bc3d9bde819253f9f565
Scott Michael Slayback <Scott.M.Slayback@navy.mil>
2019-02-21 12:41:46 -0800

    name change

--- app/src/main/java/maveric/net/MainActivity.java
+++ app/src/main/java/maveric/net/MainActivity.java
@@ -12,10 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        APICall messenger = new APICall(this, findViewById(R.id.responseView));
-
-        messenger.execute();
-
         Toast.makeText(this, R.string.api_url, Toast.LENGTH_SHORT).show();
 
         Intent svc = new Intent(this, MonitorService.class);
